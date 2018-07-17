e1c4a8e8b77c2b8672aa16db14ec8e9574ed3cff
zyy <351936866@qq.com>
2018-07-17 14:55:30 +0800

    modify compact function

--- lodash/zyycode-lodash.js
+++ lodash/zyycode-lodash.js
@@ -1,4 +1,13 @@
 var zyycode = {
+  /**
+   * Creates an array of elements split into groups the length of size. 
+   * If array can't be split evenly, the final chunk will be the remaining 
+   * elements.
+   *
+   * @param {Array} array The array to process.
+   * @param {number} size The length of each chunk.
+   * @returns {Array} Returns the new array of chunks.
+   */
   chunk: function(array, size) {
     size = size || 1
     let result = []
@@ -7,17 +16,32 @@ var zyycode = {
     }
     return result
   },
+  /**
+   * Creates an array with all falsey values removed. 
+   * The values false, null, 0, "", undefined, and NaN are falsey.
+   *
+   * @param {Array} array The array.
+   * @returns {Array} Returns the new array of the filtered values.
+   */
   compact: function(array) {
+    let result = []
     for (let i = 0; i < array.length; i++) {
-      if (!array[i]) {
-        array.splice(i, 1)
+      if (array[i]) {
+        result.push(array[i])
       }
     }
-    return array
-  },
+    return result
+  }, 
+  /**
+   * Creates a new array concatenating array with any additional arrays 
+   * and/or values.
+   *
+   * @param {Array} array The array to concatenate.
+   * @param {...*} values The values to concatenate.
+   * @returns {Array} Returns the new concatenated array.
+   */
   concat: function(array, ...values) {
     let result = array.concat(...values)
     return result
   }
 }
-console.log(zyycode.concat([1], 2, [3], [[4]]))
---
+++ test.js
@@ -0,0 +1,103 @@
+function arrayToList(array) {
+  if (array.length === 0) {
+    return null
+  }
+  let nodes = []
+  for (let i = 0; i < array.length; i++) {
+    let node = {}
+    node.value = array[i]
+    node.next = null
+    nodes.push(node)
+  }
+  for (let i = 0; i < nodes.length - 1; i++) {
+    nodes[i].next = nodes[i + 1]
+  }
+  // nodes[nodes.length - 1].next = null
+  return nodes[0]
+}
+console.log(arrayToList([1, 2, 3, 4, 5]))
+
+function arrayToList(array) {
+  let node
+  let head = {
+    value: array[0],
+    next: null,
+  }
+  let pnode = head
+
+  for (let i = 1; i < array.length; i++) {
+    node = {value: array[i], next: null}
+    pnode.next = node
+    pnode = node
+  }
+  return head
+}
+function arrayToList(array, start = 0) {
+  if (start === array.length) {
+    return null
+  }
+  let node = {
+    value: array[0],
+    next: null,
+  }
+  node.next
+  return arrayToList(array, start + 1)
+  node.next = node
+}
+
+
+function listToArray(list) {
+  if (!list) {
+    return []
+  }
+  let array = []
+  while (list.next !== null) {
+    array.push(list.value)
+    list = list.next
+  }
+  return array
+}
+
+function insert(head, n, value) {
+  let node = {
+    value: value,
+    next: null,
+  }
+  let p = head
+  for (let i = 0; i < n - 1; i++) {
+    p = p.next
+  }
+  node.next = p.next
+  p.next = node
+  return head
+}
+// 递归实现
+function insert(head, n, value) {
+  
+}
+
+function remove(head, n) {
+  if (!head) {
+    return null
+  }
+  if (n === 0) {
+    return head.next
+  }
+  let p = head
+  for (let i = 0; i < n - 1; i++) {
+    p = p.next
+  }
+  p.next = p.next.next
+  return head
+}
+  
+function forEachDigit(n, action) {
+  do {
+    var digit = n % 10
+    action(digit)
+    n = (n - digit) / 10
+  } while (n > 0)
+}
+forEachDigit(12345, d => {
+  console.log(d)
+})
